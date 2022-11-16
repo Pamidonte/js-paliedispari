b679d48be6b47b5ddbdd933afe8188fbd908a685
Pamidonte <francescomarrone8@gmail.com>
2022-11-16 22:03:13 +0100

    compito concluso

--- script.js
+++ script.js
@@ -1,29 +1,62 @@
-let parolainserita = prompt("inserisci una parola");
+/* -------------- MAIN ---------------- */
 
-let risultato = parolapalindroma(parolainserita);
+let parolainserita = prompt("inserisci una parola").toLowerCase;
+let risultato = isParolapalindroma(parolainserita);
 
-if (risultato) {
-    console.log("la parola è palindroma");
+console.log(getResultString(risultato));
+
+let sceltapariodispari = prompt("scegli pari o dispari");
+let sceltanumero = prompt("inserisci un numero da 1 a 5");
+let numeronemico = getNumerorandom();
+
+let somma = (parseInt(sceltanumero) + parseInt(numeronemico));
+
+
+
+console.log(pariodispari(somma));
+
+if (sceltapariodispari == pariodispari(somma)) {
+    console.log("hai vinto");
 
 } else {
-    console.log("la parola non è palindroma");
+    console.log("hai perso");
+
+}
+
+/* ------------------------------ */
+
+
+/* ------------- FUNZIONI ----------------- */
+
+
+
+function getResultString(risultato) {
+    if (risultato) {
+        return "la parola è palindroma";
+    } else {
+        return "la parola non è palindroma";
+    };
+}
+
+function pariodispari(somma) {
+    if (somma % 2 == 0) {
+        return "pari";
+    }
+    return "dispari";
+}
+
+function getNumerorandom() {
+    return Math.floor(Math.random() * (5 - 1 + 1) + 1);
 };
 
 
 
 
-function parolapalindroma(parola) {
+function isParolapalindroma(parola) {
     for (let i = 0; i < parola.length; i++) {
-
         if (parola[i] != parola[parola.length - 1 - i]) {
             return false;
-
         };
-
     };
     return true;
 };
-
-
-
-
